48976193aa38c7bcf9927632d1ab4458e52c7364
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 19:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,15 +14,19 @@ function divide(a, b) {
   return a / b;
 }
 
+function dec(n) {
+  return n--;
+}
+
 function inc(n) {
   return n++;
 }
 
-function dec(n) {
-  return n--;
-}
 
 function makeInt(n) {
   return parseInt(n, 10);
 }
 
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
